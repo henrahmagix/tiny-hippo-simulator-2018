dba61ff0989aa811e2c437ae43b74f641311733d
Henry Blyth <blyth.henry@gmail.com>
2018-02-11 20:28:30 +0000

    Add moving to second bg with hippo down sprite

--- src/index.js
+++ src/index.js
@@ -73,21 +73,42 @@ function showBg(position) {
 	bgs.forEach((bg, i) => bg.visible = (i === position));
 }
 
-let character;
-let allCharacters = [];
+let player;
+let direction = 'right';
+let allCharacters = {
+	down: [],
+	left: [],
+	right: [],
+	up: [],
+};
 
-function addWalking(x, y, sprite) {
-	sprite = game.add.sprite(x, y, sprite);
+const CHARACTER_FRAME_INCREMENT = 1 / 4;
+const CHARACTER_MOVE_INCREMENT = 1/2;
+
+function makeSprite(x, y, name) {
+	let sprite = game.add.sprite(x, y, name);
+	sprite.visible = false;
+	sprite.frame = 0;
+	sprite.fpsFrame = 0;
+	sprite.xFps = 0;
+	sprite.yFps = 0;
+	return sprite;
+};
+
+function addWalking(x, y, name) {
+	allCharacters.down.push(makeSprite(x, y, `${name}down`));
+	allCharacters.left.push(makeSprite(x, y, `${name}left`));
+	allCharacters.up.push(makeSprite(x, y, `${name}up`));
+
+	let sprite = makeSprite(x, y, `${name}right`);
 	sprite.animations.add('walk');
 	sprite.animations.play('walk', 4, true);
-	sprite.visible = false;
 	sprite.inputEnabled = true;
-	sprite.events.onInputDown.add(sprite => character = sprite, this);
-	allCharacters.push(sprite);
+	sprite.events.onInputDown.add(sprite => player = sprite, this);
+	allCharacters.right.push(sprite);
 	return sprite;
 }
 
-let sprite;
 let text;
 
 function create() {
@@ -97,10 +118,10 @@ function create() {
 	addBg('page1');
 	addBg('page2');
 
-	addWalking(40, 200, 'hippo1right');
-	addWalking(140, 200, 'hippo2right');
-	addWalking(240, 200, 'hippo3right');
-	addWalking(340, 200, 'hippo4right');
+	addWalking(40, 200, 'hippo1');
+	addWalking(140, 200, 'hippo2');
+	addWalking(240, 200, 'hippo3');
+	addWalking(340, 200, 'hippo4');
 
 	const style = {
 		fontWeight: "bold",
@@ -124,38 +145,87 @@ function showCharacterChoice() {
 	showing = CHARACTER_CHOICE;
 	showBg(-1);
 	text.visible = true;
-	allCharacters.forEach(c => c.visible = true);
+	allCharacters.right.forEach(c => c.visible = true);
 }
 
 function showGame() {
 	showing = GAME;
-	showBg(0);
+	showBg(showing - 1);
 	text.visible = false;
-	allCharacters.forEach(c => c.visible = false);
-	character.visible = true;
-	character.x = 40;
-	character.y = 285;
+	allCharacters.right.forEach(c => {
+		c.visible = false;
+		c.animations.stop();
+		c.frame = 0;
+	});
+	player.visible = true;
+	player.x = 40;
+	player.xFps = player.x;
+	player.y = 285;
+}
+
+function swapCharacter(from, to) {
+	let spritePos = allCharacters[from].indexOf(player);
+	player.visible = false;
+	player = allCharacters[to][spritePos];
+	player.visible = true;
+}
+
+function walk(axis, movement) {
+	player.fpsFrame = (player.fpsFrame + CHARACTER_FRAME_INCREMENT) % 4;
+	let nextFrame = Math.floor(player.fpsFrame);
+	if (nextFrame > 2) {
+		nextFrame = 1;
+	}
+	player.frame = nextFrame;
+
+	player[axis] = Math.floor(player[movement]);
+	if (bgs[0].visible && player.x + player.width > game.width) {
+		showBg(1);
+		swapCharacter('right', 'down');
+		direction = 'down';
+		player.x = 760;
+		player.xFps = 760;
+		player.y = 345;
+		player.yFps = 345;
+	} else if (bgs[1].visible && player.y < 345) {
+		showBg(0);
+		swapCharacter('down', 'right');
+		direction = 'right';
+		player.x = game.width - player.width;
+		player.xFps = game.width - player.width;
+		player.y = 285;
+		player.yFps = 285;
+	}
 }
 
 function update() {
 	const is = function (input) {
 		return game.input.keyboard.isDown(input);
 	};
-	if (character && showing == GAME) {
-		const move = 2;
+	if (player && showing == GAME) {
+		let axis;
+		if (direction === 'left' || direction === 'right') {
+			axis = 'x';
+		}
+		if (direction === 'up' || direction === 'down') {
+			axis = 'y';
+		}
+		let movement = `${axis}Fps`;
 		if (is(Phaser.Keyboard.LEFT)) {
-			character.x -= 2;
+			player[movement] = player[movement] - CHARACTER_MOVE_INCREMENT;
+			walk(axis, movement);
 		} else if (is(Phaser.Keyboard.RIGHT)) {
-			character.x += 2;
+			player[movement] = player[movement] + CHARACTER_MOVE_INCREMENT;
+			walk(axis, movement);
 		}
 	}
 }
 
 function render() {
-	if (!character && showing != CHARACTER_CHOICE) {
+	if (!player && showing != CHARACTER_CHOICE) {
 		return showCharacterChoice();
 	}
-	if (character && showing != GAME) {
+	if (player && showing != GAME) {
 		return showGame();
 	}
 }
